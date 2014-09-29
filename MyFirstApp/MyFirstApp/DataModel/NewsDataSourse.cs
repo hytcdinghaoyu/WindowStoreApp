64679d25927cfa29c41ccd17d9565381ad0ffd78
hytcdinghaoyu <835399559@qq.com>
2014-09-29 16:51:16 +0800

    firstapp
    firstapp

---
+++ MyFirstApp/MyFirstApp/DataModel/NewsDataSourse.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using Windows.Data.Json;
+using Windows.Data.Xml.Dom;
+using Windows.Data.Xml.Xsl;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+
+namespace MyFirstApp.DataModel
+{
+    public class NewsDataItem
+    {
+        public NewsDataItem(String uid, String title, String description, String imgpath, String link, String pubDate, String author)
+        {
+            this.Uid = uid;
+            this.Title = title;
+            this.Description = description;
+            this.ImgPath = imgpath;
+            this.Link = link;
+            this.PubDate = pubDate;
+            this.Author = author;
+        }
+        public NewsDataItem()
+        {
+
+        }
+        public string Uid { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+        public string Link { get; set; }
+        public string PubDate { get; set; }
+        public string Author { get; set; }
+    }
+
+    public class NewsDataGroup
+    {
+        public NewsDataGroup()
+        {
+            this.Items = new ObservableCollection<NewsDataItem>();
+        }
+        public ObservableCollection<NewsDataItem> Items { get; set; }
+
+    }
+    class NewsDataSource
+    {
+        private static NewsDataSource _sampleDataSource = new NewsDataSource();
+
+        private ObservableCollection<NewsDataGroup> _groups = new ObservableCollection<NewsDataGroup>();
+        public ObservableCollection<NewsDataGroup> Groups
+        {
+            get { return this._groups; }
+        }
+
+    }
+}
---
+++ MyFirstApp/MyFirstApp/ItemPage.xaml.cs
@@ -0,0 +1,222 @@
+﻿using MyFirstApp.Common;
+using MyFirstApp.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.StartScreen;
+
+
+
+// 有关“项页”项模板的信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=234232
+
+namespace MyFirstApp
+{
+    /// <summary>
+    /// 显示组内某一项的详细信息的页面。
+    /// </summary>
+    public sealed partial class ItemPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private StorageFile _photo; // Photo file to share
+        private StorageFile _video; // Video file to share
+        /// <summary>
+        /// NavigationHelper 在每页上用于协助导航和
+        /// 进程生命期管理
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// 可将其更改为强类型视图模型。
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        public ItemPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.SizeChanged += ItemPage_SizeChanged;
+
+        }
+
+        private void ItemPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width <= 800)
+            {
+                VisualStateManager.GoToState(this, "NarrowLayout", true);
+            }
+            else if (e.NewSize.Width > 800 && e.NewSize.Width <= 1250)
+            {
+                VisualStateManager.GoToState(this, "IntermediateLayout", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "DefaultLayout", true);
+            }
+
+        }
+
+        /// <summary>
+        /// 使用在导航过程中传递的内容填充页。  在从以前的会话
+        /// 重新创建页时，也会提供任何已保存状态。
+        /// </summary>
+        /// <param name="sender">
+        /// 事件的来源；通常为 <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">事件数据，其中既提供在最初请求此页时传递给
+        /// <see cref="Frame.Navigate(Type, Object)"/> 的导航参数，又提供
+        /// 此页在以前会话期间保留的状态的
+        /// 字典。首次访问页面时，该状态将为 null。</param>
+        private async void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            // TODO: 创建适用于问题域的合适数据模型以替换示例数据
+            var item = await SampleDataSource.GetItemAsync((String)e.NavigationParameter);
+            this.DefaultViewModel["Item"] = item;
+        }
+
+        #region NavigationHelper 注册
+
+        /// 此部分中提供的方法只是用于使
+        /// NavigationHelper 可响应页面的导航方法。
+        /// 
+        /// 应将页面特有的逻辑放入用于
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// 和 <see cref="GridCS.Common.NavigationHelper.SaveState"/> 的事件处理程序中。
+        /// 除了在会话期间保留的页面状态之外
+        /// LoadState 方法中还提供导航参数。
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+        private async void OnCapturePhoto(object sender, RoutedEventArgs e)
+        {
+            var camera = new CameraCaptureUI();
+            var file = await camera.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (file != null)
+            {
+                var item = (SampleDataItem)this.DefaultViewModel["Item"];
+                item.Media.Add(file);
+
+                _photo = file;
+                DataTransferManager.ShowShareUI();
+            }
+        }
+
+
+        private async void OnCaptureVideo(object sender, RoutedEventArgs e)
+        {
+            var camera = new CameraCaptureUI();
+            camera.VideoSettings.Format = CameraCaptureUIVideoFormat.Wmv;
+            var file = await camera.CaptureFileAsync(CameraCaptureUIMode.Video);
+
+            if (file != null)
+            {
+                var item = (SampleDataItem)this.DefaultViewModel["Item"];
+                item.Media.Add(file);
+
+                _video = file;
+                DataTransferManager.ShowShareUI();
+            }
+        }
+
+
+        void OnDataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var request = args.Request;
+            var item = (SampleDataItem)this.DefaultViewModel["Item"];
+            request.Data.Properties.Title = item.Title;
+
+            if (_photo != null)
+            {
+                request.Data.Properties.Description = "Recipe photo";
+                var reference = Windows.Storage.Streams.RandomAccessStreamReference.CreateFromFile(_photo);
+                request.Data.Properties.Thumbnail = reference;
+                request.Data.SetBitmap(reference);
+                _photo = null;
+            }
+            else if (_video != null)
+            {
+                request.Data.Properties.Description = "Recipe video";
+                List<StorageFile> items = new List<StorageFile>();
+                items.Add(_video);
+                request.Data.SetStorageItems(items);
+                _video = null;
+            }
+
+            else
+            {
+                request.Data.Properties.Description = "Recipe ingredients and directions";
+
+                // Share recipe text
+                var recipe = "\r\nINGREDIENTS\r\n";
+                recipe += String.Join("\r\n", item.Ingredients);
+                recipe += ("\r\n\r\nDIRECTIONS\r\n" + item.Content);
+                request.Data.SetText(recipe);
+
+                // Share recipe image
+                var reference = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///" + item.ImagePath));
+                request.Data.Properties.Thumbnail = reference;
+                request.Data.SetBitmap(reference);
+            }
+        }
+
+        private void OnNavigateToMedia(object sender, RoutedEventArgs e)
+        {
+            var item = (SampleDataItem)this.DefaultViewModel["Item"];
+            this.Frame.Navigate(typeof(ItemMediaPage), item.UniqueId);
+        }
+
+        private async void OnPinRecipe(object sender, RoutedEventArgs e)
+        {
+            var item = (SampleDataItem)this.DefaultViewModel["Item"];
+            var uri = new Uri("ms-appx:///" + item.TileImagePath);
+
+            var tile = new SecondaryTile(
+                item.UniqueId,
+                item.Title,
+                item.UniqueId,
+                uri,
+                TileSize.Square150x150);
+
+            tile.VisualElements.ShowNameOnSquare150x150Logo = true;
+
+            await tile.RequestCreateAsync();
+        }
+
+
+
+    }
+}
